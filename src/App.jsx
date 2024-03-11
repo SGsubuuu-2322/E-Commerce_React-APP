978f856d004c8f7f61473268c1546728ae003542
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-11 23:43:09 +0530

    Ceated card with its structure and its stylings....

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,18 @@ const App = () => {
         </ul>
       </nav>
 
-      <div className="h-full w-[85%] bg-red-100"></div>
+      <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
+        <div className="card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+            style={{
+              backgroundImage:
+                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
+            }}
+          ></div>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+        </div>
+      </div>
     </div>
   );
 };
