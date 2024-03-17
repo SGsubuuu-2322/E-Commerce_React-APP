c2ee964757d78498ff892a4884f345c093492756
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-17 18:00:04 +0530

    Setup the context for products for our app by Outsourcing the Axios object...

---
+++ src/Utils/Context.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+
+import axios from "./Axios";
+import { createContext, useEffect, useState } from "react";
+
+export const productContext = createContext();
+
+const Context = ({ children }) => {
+  const [products, setProducts] = useState(null);
+
+  const getProducts = async () => {
+    try {
+      const data = await axios("/products");
+      setProducts(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <productContext.Provider value={[products, setProducts]}>
+      {children}
+    </productContext.Provider>
+  );
+};
+
+export default Context;
