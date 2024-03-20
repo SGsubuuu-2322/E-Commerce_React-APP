480b2efc7ae70fe0b0ebe495e6181b5246d7f4e0
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-20 11:33:57 +0530

    Added the category filter feature in our app...

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,35 +1,56 @@
 // import React from 'react'
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Nav from "./Nav";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Loading from "./Loading";
 import { productContext } from "../Utils/Context";
+import axios from "../Utils/Axios";
 
 const Home = () => {
   const [products] = useContext(productContext);
+  const [filteredProducts, setFilteredProducts] = useState(null);
 
-  // console.log(products);
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
+  // console.log(category);
+
+  const getProductsCategory = async () => {
+    try {
+      const { data } = await axios.get(`/products/category/${category}`);
+      setFilteredProducts(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!filteredProducts) setFilteredProducts(products);
+    if (category !== "undefined") getProductsCategory();
+  }, [category, products]);
+
+  // console.log(filteredProducts);
 
   return products ? (
     <>
       <Nav />
       <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        {products.map((p, i) => (
-          <Link
-            key={i}
-            to={`/details/${p.id}`}
-            className="card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center"
-          >
-            <div
-              className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-              style={{
-                backgroundImage: `url(${p.image})`,
-              }}
-            ></div>
-            <h1 className="hover:text-blue-300">{p.title}</h1>
-          </Link>
-        ))}
+        {filteredProducts &&
+          filteredProducts.map((p, i) => (
+            <Link
+              key={i}
+              to={`/details/${p.id}`}
+              className="card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex flex-col justify-center items-center"
+            >
+              <div
+                className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+                style={{
+                  backgroundImage: `url(${p.image})`,
+                }}
+              ></div>
+              <h1 className="hover:text-blue-300">{p.title}</h1>
+            </Link>
+          ))}
       </div>
     </>
   ) : (
